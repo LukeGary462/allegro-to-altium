564b9b9f2f5e0923a9874e56b6b13385eaecc4f2
Luke Gary <luke.gary@solstice.com>
2025-10-23 19:30:28 -0400

    starting python version to get a little more fancy

---
+++ allegro2altium.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# python 3
+#pylint: disable=
+##    @file:    allegro2altium.py
+#     @name:    Luke Gary
+#     @date:    2025/10/23
+
+"""
+{ high-level module description }
+"""
