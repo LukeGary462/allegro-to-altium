af607d4151f2aa1ad91e9efd24a9822b8e80a776
Luke Gary <luke.gary@solstice.com>
2025-10-23 19:34:39 -0400

    python skelleton

--- allegro2altium.py
+++ allegro2altium.py
@@ -8,3 +8,25 @@
 """
 { high-level module description }
 """
+
+from argparse import ArgumentParser
+
+def get_args():
+    parser = ArgumentParser(description='Allegro-To-Altium Importable PCB Translator, converts .BRD to .ALG, then manually import to altium')
+    parser.add_argument('files', nargs='+', help='File list of Allegro PCB design files as *.brd or *.dra')
+    return parser.parse_args()
+
+def run_dbdoctor(filename: str = None) -> str:
+    pass
+
+def convert_to_alg(filename: str = None) -> str:
+    pass
+
+def process_files(filelist: list[str] = []):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
